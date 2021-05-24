34df4077cde903a5ac5d045bd3e895ef9e348d3e
dylancrossan231 <45007106+dylancrossan231@users.noreply.github.com>
2021-05-24 18:51:53 +0100

    first commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,30 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allWpPost {
+        nodes {
+          id
+          uri
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    reporter.error("There was an error fetching posts", result.errors)
+  }
 
-// You can delete this file if you're not using it
+  const { allWpPost } = result.data
+  console.log(result.data)
+  // Define the template to use
+  const template = require.resolve(`./src/templates/WpPosts.js`)
+
+  if (allWpPost.nodes.length) {
+    allWpPost.nodes.map(post => {
+      actions.createPage({
+        path: post.id,
+        component: template,
+        context: post,
+      })
+    })
+  }
+}
---
+++ src/templates/WpPosts.js
@@ -0,0 +1,24 @@
+import React from "react"
+import {graphql,StaticQuery} from "gatsby"
+const WpPost = ({
+  data: {
+    wpPost: { title, content, id },
+  },
+}) => {
+  return (
+    <div className={`post-${id}`}>
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  )
+}
+export const query = StaticQuery(graphql`
+  query($id: String) {
+    wpPost(id: { eq: $id }) {
+      id
+      title
+      content
+    }
+  }
+`)
+export default WpPost
